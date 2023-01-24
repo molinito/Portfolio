1fe7353edc17f0d208cd2ae1e9b6a59ad8146a4a
Marcelo Saravia <molinito48@gmail.com>
2023-01-24 20:44:26 -0300

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+
+export default function About() {
+    return (
+        <section id="about">
+       
+          <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                Hola, mi nombre es Marcelo.
+                </h1>
+                <br className="hidden lg:inline-block" />
+                <h1 className="title-font sm:text-3xl text-2xl mb-4 font-medium text-white">
+                Me apasiona la tecnología y la creación de aplicaciones.
+                
+              </h1>
+              <p className="mb-8 leading-relaxed">
+                Estoy en constante aprendizaje y me encanta compartir mis conocimientos. 
+                Recién graduado de la carrera de Fullstack Developer en la Academia Henry. Búscalo en
+                {/*crear un llink referencia a soyhenry.com*/ }
+                <a href="https://www.soyhenry.com/" className="text-green-500"> SoyHenry</a>.
+                <br className="hidden lg:inline-block" />
+                Vengo a integrarme al mundo IT ofreciendo mis servicios para satisfacer 
+                las necesidades de las empresas y clientes usuarios.
+                Este es mi portafolio, espero que lo disfrutes.
+                </p>
+              <div className="flex justify-center">
+                <a
+                  href="#contact"
+                  className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                  Contáctame
+                </a>
+                <a
+                  href="#projects"
+                  className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                  Mis proyectos
+                </a>
+              </div>
+            </div>
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+              <img
+                className="object-cover object-center rounded"
+                alt="hero"
+                src="./pngegg30.png"
+              />
+            </div>
+          </div>
+        </section>
+      );
+    }
---
+++ src/data.js
@@ -0,0 +1,119 @@
+export const skills = [
+  // incluir logos de las tecnologias que voy a usar
+  <div className="flex flex-wrap -m-4">
+     <img alt="javascript"
+     src="https://img.icons8.com/color/48/000000/javascript.png"/>
+     Javascript
+  </div>,
+  <div className="flex flex-wrap -m-4">
+      <img alt="react"
+      src="https://img.icons8.com/color/48/000000/react-native.png"/>
+      React
+  </div>,
+  <div className="flex flex-wrap -m-4">
+      <img alt="node"
+      src="https://img.icons8.com/color/48/000000/nodejs.png" style={{background:"white"}}/>
+      Node
+  </div>,
+  <div className="flex flex-wrap -m-4">
+      <img alt="express" 
+      src="https://img.icons8.com/color/48/000000/express.png" style={{background:"green"}}/>
+      Express
+  </div>,
+  <div className="flex flex-wrap -m-4">
+      <img alt="postgresql"
+      src="https://img.icons8.com/color/48/000000/postgreesql.png"/>
+      PostgreSQL
+  </div>,
+  <div className="flex flex-wrap -m-4">
+      <img alt="html"
+      src="https://img.icons8.com/color/48/000000/html-5.png"/>
+      HTML
+  </div>,
+  <div className="flex flex-wrap -m-4">
+      <img alt="css"
+      src="https://img.icons8.com/color/48/000000/css3.png"/>
+      CSS
+  </div>,
+  <div className="flex flex-wrap -m-4">
+      <img alt="bootstrap"
+      src="https://img.icons8.com/color/48/000000/bootstrap.png"/>
+      Bootstrap
+  </div>,
+  <div className="flex flex-wrap -m-4">
+      <img alt="tailwind"
+      src="./tailwind.ico" style={{width:"40px", height:"40px"}}/>
+      Tailwind
+  </div>,
+  <div className="flex flex-wrap -m-4">
+      <img alt="github"
+      src="https://img.icons8.com/color/48/000000/github--v1.png"/>
+      GitHub
+  </div>,
+  <div className="flex flex-wrap -m-4">
+      <img alt="redux"
+      src="https://img.icons8.com/color/48/000000/redux.png"/>
+      Redux
+  </div>,
+  <div className="flex flex-wrap -m-4">
+      <img alt="sequelize"
+      src="./sequelize.png"/>
+      Sequelize
+  </div>,
+  
+   
+];
+
+
+export const projects = [
+    {
+      title: "ACMEtronics",
+      subtitle: "Tienda de tecnología",
+      description:
+      "Proyecto final del curso de React de Henry. Tienda de tecnología con carrito de compras, método de pagos, registro x mail, filtros, paginado, ordenamiento, etc. Se utilizó React, Redux, Node, Express, Sequelize, PostgreSQL, Tailwind, Bootstrap, etc.",
+      image: "./19.gif",
+      link: "https://front-acmetronic-production.up.railway.app/",
+    },
+    {
+      title: "Fast Wheather",
+      subtitle: "Pronóstico climatico",
+      description: "Pequeño proyecto hecho con React. Se utiliza la API de Openweathermap para obtener el pronóstico climático de una ciudad. Se puede buscar por nombre de ciudad.",
+      image: "./31.gif",
+      link: "https://reedbarger.com",
+    },
+   
+  ];
+
+  export const testimonials = [
+    {
+      quote:
+      <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7022256808715010048" height="450" width="100%" frameborder="0"  title="Publicación integrada"></iframe>,
+      image: "./Pau.jpeg",
+      name: "Paula Kunning",
+      company: <a href="https://www.soyhenry.com/" className="text-yellow-500"><h6>Soy Henry</h6>
+      <img src="./HenryLogo.jpg" alt="logo" className="w-6 inline-block" style={{width:"20%"}} />
+      </a>,
+      link: "https://linkedin.com/in/paula-kunning",
+    },
+    {
+      quote:
+      <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7023297202127036416" height="450" width="100%" frameborder="0" allowfullscreen="" title="Publicación integrada"></iframe>,
+      image: "./Erika.jpeg",
+      name: "Erika Pineda",
+      company: <a href="https://www.soyhenry.com/" className="text-yellow-500"><h6>Soy Henry</h6>
+      <img src="./HenryLogo.jpg" alt="logo" className="w-6 inline-block" style={{width:"20%"}} />
+      </a>,
+      link: "https://linkedin.com/in/erika-pineda-92322b262",
+    },
+    {
+      quote:
+      <img alt="perfil" src={"./04.png"} height="500" width="450" frameborder="100%" allowfullscreen="" title="Publicación integrada"></img>,
+      image: "./Gian.jpeg",
+      name: "Gian Franco Zabarino",
+      company: <a href="https://trupropel.com/" className="text-yellow-500"> <h6>Trupropel</h6>
+      <img src="./trupropel.jpeg" alt="logo" className="w-6 inline-block" style={{width:"20%"}} />
+      </a> ,
+      link:"https://linkedin.com/in/gfzabarino",
+    },
+  ];
+
